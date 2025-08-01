0193b3ef92501cb98323a0502dfc6cdf6cedd9dd
AKToran <aktoran60@gmail.com>
2025-08-01 23:51:56 +0600

    basic login and register form

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router";
 
-const Login = () => {
+const Register = () => {
   return (
     <div className="card bg-base-100 w-full max-w-sm mx-auto my-12 shrink-0 shadow-2xl">
       <div className="card-body">
-      <h1 className="text-4xl text-center font-bold">Login now!</h1>
+      <h1 className="text-4xl text-center font-bold">Register now!</h1>
         <form className="fieldset">
           <label className="label">Email</label>
           <input type="email" className="input" placeholder="Email" />
           <label className="label">Password</label>
           <input type="password" className="input" placeholder="Password" />
           <div>
-            <a className="link link-hover">Forgot password?</a>
+            <Link to={'/login'} className="link link-hover">Already have an account? </Link>
           </div>
-          <button className="btn btn-neutral mt-4">Login</button>
+          <button className="btn btn-neutral mt-4">Register</button>
         </form>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default Register;
