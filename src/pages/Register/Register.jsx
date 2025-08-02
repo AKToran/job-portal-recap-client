f21d462dcd04c1c79b0d249620d66789ce82d762
AKToran <aktoran60@gmail.com>
2025-08-02 22:16:36 +0600

    lottie animation on register page

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,21 +1,31 @@
+import Lottie from "lottie-react";
 import React from "react";
 import { Link } from "react-router";
+import register from '../../assets/register.json'
 
 const Register = () => {
   return (
-    <div className="card bg-base-100 w-full max-w-sm mx-auto my-12 shrink-0 shadow-2xl">
-      <div className="card-body">
-      <h1 className="text-4xl text-center font-bold">Register now!</h1>
-        <form className="fieldset">
-          <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
-          <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
-          <div>
-            <Link to={'/login'} className="link link-hover">Already have an account? </Link>
-          </div>
-          <button className="btn btn-neutral mt-4">Register</button>
-        </form>
+    <div className="flex flex-col md:flex-row justify-center items-center md:gap-12 my-8 md:my-12">
+      <div className="w-1/3">
+        <Lottie animationData={register} loop={true} />
+      </div>
+
+      <div className="card bg-base-100 max-w-sm shrink-0 shadow-2xl w-2/3">
+        <div className="card-body">
+          <h1 className="text-4xl text-center font-bold">Register now!</h1>
+          <form className="fieldset">
+            <label className="label">Email</label>
+            <input type="email" name="email" className="input" placeholder="Email" />
+            <label className="label">Password</label>
+            <input type="password" name="password" className="input" placeholder="Password" />
+            <div>
+              <Link to={"/login"} className="link link-hover">
+                Already have an account?
+              </Link>
+            </div>
+            <button className="btn btn-neutral mt-4">Register</button>
+          </form>
+        </div>
       </div>
     </div>
   );
