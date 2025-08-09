386a8142156efdf87132711fc8c62aa421379c2f
AKToran <aktoran60@gmail.com>
2025-08-09 23:45:34 +0600

    hot jobs lay out and job cart

--- src/pages/Home/HotJobs.jsx
+++ src/pages/Home/HotJobs.jsx
@@ -1,11 +1,17 @@
 import React, { use } from 'react';
+import JobCard from '../Shared/JobCard';
 
 const HotJobs = ({jobsPromise}) => {
   const jobs = use(jobsPromise)
 
   return (
     <div>
-      {jobs.length}
+      <h1 className='text-center text-4xl my-5 font-semibold'>Hot Jobs of the Day:</h1>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4'>
+      {
+        jobs.map(job=> <JobCard key={job._id} job={job} />)
+      }
+    </div>
     </div>
   );
 };
