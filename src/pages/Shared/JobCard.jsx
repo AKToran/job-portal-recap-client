32a809cf74411cd8c3d427ac832546bcd39009d3
AKToran <aktoran60@gmail.com>
2025-08-09 23:59:12 +0600

    job card info

--- src/pages/Shared/JobCard.jsx
+++ src/pages/Shared/JobCard.jsx
@@ -1,26 +1,40 @@
 import React from "react";
+import { FaMapMarkerAlt } from "react-icons/fa";
 
-const JobCard = ({job}) => {
+const JobCard = ({ job }) => {
+  const {
+    title,
+    location,
+    jobType,
+    category,
+    description,
+    company,
+    company_logo,
+  } = job;
+
   return (
-    <div className="card bg-base-100shadow-sm">
-      <figure>
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes"
-        />
-      </figure>
+    <div className="card bg-base-100 shadow-sm">
       <div className="card-body">
+        <div className="flex gap-2">
+          <figure>
+            <img src={company_logo} alt="logo" />
+          </figure>
+          <div>
+            <h3 className="text-2xl font-semibold">{company}</h3>
+            <div className="flex items-center gap-0.5">
+              {" "}
+              <FaMapMarkerAlt /> {location}
+            </div>
+          </div>
+        </div>
         <h2 className="card-title">
-          Card Title
-          <div className="badge badge-secondary">NEW</div>
+          {title}
+          <div className="badge badge-secondary">{category}</div>
+          <div className="badge badge-secondary">{jobType}</div>
         </h2>
-        <p>
-          A card component has a figure, a body part, and inside body there are
-          title and actions parts
-        </p>
+        <p>{description}</p>
         <div className="card-actions justify-end">
-          <div className="badge badge-outline">Fashion</div>
-          <div className="badge badge-outline">Products</div>
+          <button className="btn btn-info font-bold">Details</button>
         </div>
       </div>
     </div>
