5ea96f21680264d179776b6839fdd6acbb5aad67
AKToran <aktoran60@gmail.com>
2025-08-12 22:19:00 +0600

    custom hook useAuth

--- src/pages/Home/HotJobs.jsx
+++ src/pages/Home/HotJobs.jsx
@@ -5,7 +5,7 @@ const HotJobs = ({jobsPromise}) => {
   const jobs = use(jobsPromise)
 
   return (
-    <div>
+    <div className='my-12'>
       <h1 className='text-center text-4xl my-5 font-semibold'>Hot Jobs of the Day:</h1>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4'>
       {
--- src/pages/JobApply/JobApply.jsx
+++ src/pages/JobApply/JobApply.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useParams } from 'react-router';
+import useAuth from '../../hooks/useAuth';
 
 const JobApply = () => {
+  const { id:jobId } = useParams();
+  const { user } = useAuth();
+
   return (
     <div>
-      <h3 className="text-3xl">Apply for Job: </h3>
+      <h3 className="text-3xl">Apply for Job: {jobId}</h3>
+      <p>Job seeker: {user.email}</p>
     </div>
   );
 };
