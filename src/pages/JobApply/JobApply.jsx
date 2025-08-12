8647c4a1f331f155065451e19dad356ea1b1d9da
AKToran <aktoran60@gmail.com>
2025-08-12 23:20:09 +0600

    job apply form handler

--- src/pages/JobApply/JobApply.jsx
+++ src/pages/JobApply/JobApply.jsx
@@ -1,29 +1,39 @@
 import React from "react";
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router";
 import useAuth from "../../hooks/useAuth";
 
 const JobApply = () => {
   const { id: jobId } = useParams();
   const { user } = useAuth();
 
+  const handleApplyForm = (e) =>{
+    e.preventDefault();
+    const form = e.target;
+    const linkedIn = form.linkedIn.value;
+    const github = form.github.value;
+    const resume = form.resume.value;
+    const email = user.email;
+
+    console.log(linkedIn, github, resume, email);
+  }
+
   return (
     <div>
-      <h3 className="text-3xl">Apply for Job: {jobId}</h3>
-      <p>Job seeker: {user.email}</p>
 
-      <form className="flex flex-col justify-center items-center bg-base-200 border-base-300 rounded-box border p-6 my-8">
+      <form onSubmit={handleApplyForm} className="flex flex-col justify-center items-center bg-base-200 border-base-300 rounded-box border p-6 my-8">
+      <h3 className="text-2xl">Apply for Job: <Link className="link link-primary" to={`/jobs/${jobId}`}>Details</Link> </h3>
         <h1 className="text-2xl font-semibold my-3">Applier information:</h1>
         <div>
           <label className="label">LinkedIn Profile</label> <br />
-          <input type="url" className="input w-80" placeholder="linkedIn profile link" />
+          <input type="url" name="linkedIn" className="input w-80" placeholder="linkedIn profile link" />
         </div>
         <div>
           <label className="label">Github Profile</label> <br />
-          <input type="url" className="input w-80" placeholder="github profile link" />
+          <input type="url" name="github" className="input w-80" placeholder="github profile link" />
         </div>
         <div>
           <label className="label">Resume</label> <br />
-          <input type="text" className="input w-80" placeholder="resume link" />
+          <input type="text" name="resume" className="input w-80" placeholder="resume link" />
         </div>
 
         <input className="btn my-3 w-80 font-semibold" type="submit" value="Submit" />
