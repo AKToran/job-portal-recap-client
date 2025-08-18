5012c1a1a7135fa4565d63455ff8cfb647e6009f
AKToran <aktoran60@gmail.com>
2025-08-18 23:18:53 +0600

    job list in myapplications page

--- src/pages/MyApplications/ApplicationList.jsx
+++ src/pages/MyApplications/ApplicationList.jsx
@@ -1,11 +1,29 @@
-import React, { use } from 'react';
+import React, { use } from "react";
+import { Link } from "react-router";
 
-const ApplicationList = ({applicationsPromise}) => {
+const ApplicationList = ({ applicationsPromise }) => {
   const applications = use(applicationsPromise);
   return (
-    <div>
-      list
-      {applications.length}
+    <div className="overflow-x-auto">
+      <table className="table table-zebra">
+        {/* head */}
+        <thead>
+          <tr>
+            <th></th>
+            <th>Job Id</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {applications.map((app,i) => (
+            <tr key={i}>
+              <th>{i+1}</th>
+              <td>{app.jobId}</td>
+              <td><Link className="link-info link-hover" to={`/jobs/${app.jobId}`}>Details</Link></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
