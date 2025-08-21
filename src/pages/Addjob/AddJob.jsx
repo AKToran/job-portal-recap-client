3b77948630b87349da9c546840ae2f0c37f0b625
AKToran <aktoran60@gmail.com>
2025-08-21 23:25:38 +0600

    job salary and description

--- src/pages/Addjob/AddJob.jsx
+++ src/pages/Addjob/AddJob.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import useAuth from "../../hooks/useAuth";
 
 const AddJob = () => {
+  const { user } = useAuth();
   return (
     <form>
       
@@ -34,12 +36,66 @@ const AddJob = () => {
       <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
         <legend className="fieldset-legend">Job Category</legend>
         <select defaultValue="Job Category" name="category" className="select">
-          <option disabled={true}>Pick a color</option>
+          <option disabled={true}>Job Category</option>
           <option>Engineering</option>
           <option>Marketing</option>
           <option>UI/UX</option>
         </select>
       </fieldset>
+
+      {/* Salary Range */}
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full border p-4">
+          <legend className="fieldset-legend">Salary Range</legend>
+
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+            <div>
+              <label className="label">Minimum Salary</label>
+              <input
+                type="text"
+                name="min"
+                className="input"
+                placeholder="Minimum Salary"
+              />
+            </div>
+
+            <div>
+              <label className="label">Maximum Salary</label>
+              <input
+                type="text"
+                name="max"
+                className="input"
+                placeholder="Maximum Salary"
+              />
+            </div>
+
+            <div>
+              <label className="label">Currency</label> <br />
+              <select
+                defaultValue="Select a Currency"
+                name="currency"
+                className="select"
+              >
+                <option disabled={true}>Select a Currency</option>
+                <option>BDT</option>
+                <option>USD</option>
+                <option>EU</option>
+              </select>
+            </div>
+          </div>
+        </fieldset>
+
+        {/* Job Description  */}
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full border p-4">
+          <legend className="fieldset-legend">Job Description</legend>
+          <textarea
+            className="textarea w-full"
+            name="description"
+            placeholder="Job Description"
+          ></textarea>
+        </fieldset>
+
+        
+
 
     </form>
   );
